d1e16c4d87f5241c3d4371d4d47650ae6edc30c4
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2019-09-11 15:10:03 -0300

    chore(): implements reverse urls for cleaner tests

--- testusa/core/tests/test_views.py
+++ testusa/core/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
 class AverageTemperatureTestCase(TestCase):
@@ -7,21 +8,21 @@ class AverageTemperatureTestCase(TestCase):
 
     def test_invalid_sources_raises_bad_request(self):
         resp = self.c.get(
-            '/api/average-temperature/123/123/',
+            reverse('average-temperature', kwargs={'lat': 123, 'lon': 123}),
             {'sources': 'thissourcedoesnotexist'}
         )
         self.assertEqual(resp.status_code, 400)
 
     def test_empty_sources_raises_bad_request(self):
         resp = self.c.get(
-            '/api/average-temperature/123/123/',
+            reverse('average-temperature', kwargs={'lat': 123, 'lon': 123}),
             {'sources': ''}
         )
         self.assertEqual(resp.status_code, 400)
 
     def test_invalid_coordinates_raises_bad_request(self):
         resp = self.c.get(
-            '/api/average-temperature/91/181/',
+            reverse('average-temperature', kwargs={'lat': 91, 'lon': 181}),
             {'sources': 'noaa'}
         )
         self.assertEqual(resp.status_code, 400)
--- testusa/core/urls.py
+++ testusa/core/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from .views import AverageTemperatureView
 
 urlpatterns = [
-        path('average-temperature/<str:lat>/<str:lon>/', AverageTemperatureView.as_view()),
+        path(
+            'average-temperature/<str:lat>/<str:lon>/',
+            AverageTemperatureView.as_view(),
+            name='average-temperature'
+        ),
 ]
